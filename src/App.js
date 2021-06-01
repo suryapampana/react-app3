4d3d194e4bf9f5548e12ff4a656631075b16fbe6
suryapampana <17pa5a0320@vishnu.edu.in>
2021-06-01 11:31:25 +0530

    bootstrap Accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Alert, Butto } from "react-bootstrap";
+import { Accordion, Alert, Button, Card } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <h1 className="bg-primary text-light p-2"> React Bootstrap</h1>
-      <Button variant="secondary">React Bootstrap </Button>
+      <h1 className="bg-primary text-light p-2">React Bootstrap</h1>
 
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
-        aliquam vero quia eius maxime laborum deleniti officiis nostrum alias
-        accusantium reprehenderit, ex eveniet inventore tempora nobis, beatae
-        doloribus! Eos, aliquid.
+      <h2>React Bootstrap Accordion</h2>
+      <Accordion defaultActiveKey="0">
+        {[1, 2, 3].map((item, index) => (
+          <Card key={item}>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey={index + 1}>
+                Vechile {index + 1}
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={index + 1}>
+              <Card.Body>Hello! I'm the body</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        ))}
+      </Accordion>
+
+      <br />
+      <br />
+      <h3 className="text-info">Bootstrap Accordion</h3>
+      <div class="accordion" id="accordionExample">
+        <div class="card">
+          <div class="card-header" id="headingOne">
+            <h2 class="mb-0">
+              <button
+                class="btn btn-link btn-block text-left"
+                type="button"
+                data-toggle="collapse"
+                data-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                Collapsible Group Item #1
+              </button>
+            </h2>
+          </div>
+
+          <div
+            id="collapseOne"
+            class="collapse show"
+            aria-labelledby="headingOne"
+            data-parent="#accordionExample"
+          >
+            <div class="card-body">
+              Some placeholder content for the first accordion panel. This panel
+              is shown by default, thanks to the <code>.show</code> class.
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingTwo">
+            <h2 class="mb-0">
+              <button
+                class="btn btn-link btn-block text-left collapsed"
+                type="button"
+                data-toggle="collapse"
+                data-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                Collapsible Group Item #2
+              </button>
+            </h2>
+          </div>
+          <div
+            id="collapseTwo"
+            class="collapse"
+            aria-labelledby="headingTwo"
+            data-parent="#accordionExample"
+          >
+            <div class="card-body">
+              Some placeholder content for the second accordion panel. This
+              panel is hidden by default.
+            </div>
+          </div>
+        </div>
       </div>
-
-      <Alert variant="primary">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti ut
-        inventore consectetur voluptates aut earum unde culpa officiis ratione
-        vel.
-      </Alert>
     </div>
   );
 }
