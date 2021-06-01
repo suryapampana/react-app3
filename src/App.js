f811a04a57f1a2f329c9f64c7552a6831c9582ae
suryapampana <17pa5a0320@vishnu.edu.in>
2021-06-01 16:49:37 +0530

    creating pages using router

--- src/App.js
+++ src/App.js
@@ -6,19 +6,53 @@ import { Button, ButtonGroup, Col, Row } from "react-bootstrap";
 import { Member1 } from "./views/Member1";
 import { Member2 } from "./views/Member2";
 import { Member3 } from "./views/Member3";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // TEAM LEADER
 function App() {
   return (
-    <div className="container-fluid">
-      <div className="row" style={{ height: "100vh" }}>
-        <div className="col-12 col-md-6 bg-dark text-light d-flex align-items-center justify-content-center">
-          <Member1></Member1>
-        </div>
-        <div className="col-12 col-md-6 bg-secondary text-light d-flex align-items-center justify-content-center">
-          <Member2></Member2>
-        </div>
-      </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Member1 />
+        </Route>
+
+        <Route path="/page1" exact>
+          <Member1 />
+        </Route>
+
+        <Route path="/page2" exact>
+          <Member2 />
+        </Route>
+
+        <Route path="/page3" exact>
+          <Member3 />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Page1() {
+  return (
+    <div>
+      <h1>Page1</h1>
+    </div>
+  );
+}
+
+function Page2() {
+  return (
+    <div>
+      <h1>Page2</h1>
+    </div>
+  );
+}
+
+function Page3() {
+  return (
+    <div>
+      <h1>Page3</h1>
     </div>
   );
 }
