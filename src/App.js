2d04f2001f85cf2d3ee52570d2ef11140619097d
suryapampana <17pa5a0320@vishnu.edu.in>
2021-06-01 10:13:11 +0530

    React Bootstrap with alert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Alert, Butto } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="bg-primary text-light p-2"> React Bootstrap</h1>
+      <Button variant="secondary">React Bootstrap </Button>
+
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+        aliquam vero quia eius maxime laborum deleniti officiis nostrum alias
+        accusantium reprehenderit, ex eveniet inventore tempora nobis, beatae
+        doloribus! Eos, aliquid.
+      </div>
+
+      <Alert variant="primary">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti ut
+        inventore consectetur voluptates aut earum unde culpa officiis ratione
+        vel.
+      </Alert>
     </div>
   );
 }
