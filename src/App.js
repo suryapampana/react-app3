d401e2a99a2e505d8a77ebdee079a975b5c92776
suryapampana <17pa5a0320@vishnu.edu.in>
2021-06-01 11:37:32 +0530

    colums division

--- src/App.js
+++ src/App.js
@@ -1,86 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {
-  Button,
-  Carousel,
-  Form,
-  FormControl,
-  Modal,
-  Nav,
-  Navbar,
-  NavDropdown,
-} from "react-bootstrap";
+
 import { useState } from "react";
 
 function App() {
-  const [modalShow, setModalShow] = useState(false);
-  const displayModal = () => setModalShow(!modalShow);
-
   return (
-    <div>
-      <Navbar bg="primary" expand="lg" variant="dark">
-        <Navbar.Brand href="#home">EMS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
-      {[1, 2, 2, 3, 4].map((item, index) => (
-        <div className="mb-1" key={index}>
-          <Button variant="secondary" onClick={() => displayModal()}>
-            Employee Details {index + 1}
-          </Button>
-        </div>
-      ))}
-
-      <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      ></MyVerticallyCenteredModal>
+    <div className="row" style={{ height: "100vh" }}>
+      <div className="d-none d-md-block    col-md-3  bg-primary"></div>
+      <div className="col-12 col-md-6 bg-dark"></div>
+      <div className="d-none d-md-block   col-md-3 bg-info"></div>
     </div>
-  );
-}
-
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
   );
 }
 
