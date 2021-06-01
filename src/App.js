3282a22a38e2866956dfd396f64ca19079e99766
suryapampana <17pa5a0320@vishnu.edu.in>
2021-06-01 16:31:40 +0530

    registration module

--- src/App.js
+++ src/App.js
@@ -2,15 +2,151 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Button, ButtonGroup, Col, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <Row style={{ height: "100vh" }}>
-      <Col xs={0} md={3} className="bg-primary"></Col>
-      <Col sm={12} md={6} className="bg-dark"></Col>
-      <Col xs={0} md={3} className="bg-info"></Col>
-    </Row>
+    <div>
+      <Member1 />
+
+      <div className="mb-5"></div>
+      <Member2 />
+
+      <div className="mb-5"></div>
+      <Member3 />
+    </div>
+  );
+}
+
+function Member1() {
+  return (
+    <div>
+      <h1>Member 1 - Registeration</h1>
+      <div>
+        <div className="mb-1">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Username"
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Password"
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Mobile"
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-1">
+          <input
+            type="button"
+            value="Register"
+            className="btn btn-primary w-100"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Member2() {
+  return (
+    <div>
+      <h1>Member 2 - Login Module</h1>
+      <div className="mb-1">
+        <input
+          type="text"
+          placeholder="Enter Username"
+          className="form-control"
+        />
+      </div>
+
+      <div className="mb-1">
+        <input
+          type="text"
+          placeholder="Enter Password"
+          className="form-control"
+        />
+      </div>
+
+      <div>
+        <Button className="w-100">Login Here</Button>
+      </div>
+    </div>
+  );
+}
+
+function Member3() {
+  return (
+    <div>
+      <h1>Forgot Password Module</h1>
+
+      <div className="mb-1">
+        <input
+          type="text"
+          placeholder="Enter Username"
+          className="form-control"
+        />
+      </div>
+
+      <div className="mb-1">
+        <input
+          type="text"
+          placeholder="Old Password"
+          className="form-control"
+        />
+      </div>
+
+      <div className="mb-1">
+        <input
+          type="text"
+          placeholder="New Password"
+          className="form-control"
+        />
+      </div>
+
+      <div className="mb-1">
+        <input
+          type="text"
+          placeholder="Re Enter New Password"
+          className="form-control"
+        />
+      </div>
+
+      <div>
+        <input
+          type="button"
+          value="Reset Password"
+          className="btn btn-primary w-100"
+        />
+      </div>
+    </div>
   );
 }
 
