62982678f587f03fdd0b7966f0e381663127f5a8
suryapampana <17pa5a0320@vishnu.edu.in>
2021-06-01 11:36:43 +0530

    modal show navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, Carousel, Modal } from "react-bootstrap";
+import {
+  Button,
+  Carousel,
+  Form,
+  FormControl,
+  Modal,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 import { useState } from "react";
 
 function App() {
@@ -9,7 +18,27 @@ function App() {
 
   return (
     <div>
-      <h1 className="bg-dark text-light p-2">Modal demo</h1>
+      <Navbar bg="primary" expand="lg" variant="dark">
+        <Navbar.Brand href="#home">EMS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
       {[1, 2, 2, 3, 4].map((item, index) => (
         <div className="mb-1" key={index}>
