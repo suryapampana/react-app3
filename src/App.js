7e72bfea873add2977bda015234b6214592d7f3f
suryapampana <17pa5a0320@vishnu.edu.in>
2021-06-01 11:34:32 +0530

    display Modal

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Carousel } from "react-bootstrap";
+import { Button, Carousel, Modal } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [modalShow, setModalShow] = useState(false);
+  const displayModal = () => setModalShow(!modalShow);
+
   return (
     <div>
-      <h1 className="bg-dark text-light p-2">Carousel demo</h1>
+      <h1 className="bg-dark text-light p-2">Modal demo</h1>
 
-      <Carousel>
-        {[1, 2, 3].map((item, index) => (
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={"https://picsum.photos/100" + index}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      <Button variant="secondary" onClick={() => displayModal()}>
+        Open Modal
+      </Button>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      ></MyVerticallyCenteredModal>
     </div>
+  );
+}
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
