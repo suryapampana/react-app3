af19e600db144ad0fe7dc370c73816cb57240080
suryapampana <17pa5a0320@vishnu.edu.in>
2021-06-01 11:38:39 +0530

    columns

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { useState } from "react";
+import { Col, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="row" style={{ height: "100vh" }}>
-      <div className="d-none d-md-block    col-md-3  bg-primary"></div>
-      <div className="col-12 col-md-6 bg-dark"></div>
-      <div className="d-none d-md-block   col-md-3 bg-info"></div>
-    </div>
+    <Row style={{ height: "100vh" }}>
+      <Col xs={0} md={3} className="bg-primary"></Col>
+      <Col sm={12} md={6} className="bg-dark"></Col>
+      <Col xs={0} md={3} className="bg-info"></Col>
+    </Row>
   );
 }
 
